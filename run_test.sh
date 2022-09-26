f47ca8357ee4bb301378f72c823ec3fe0832701c
laczkol <nagyonlevente@gmail.com>
2022-09-26 08:55:57 +0200

    updated test run

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-./QVG.sh -r ./test/ref.fa -samples-list ./test/slist -s ./test -o ./test -annot yes -g ./test/ref.fa -np 1
+mkdir test/output
+
+./QVG.sh -r ./test/ref.fa -samples-list ./test/slist -s ./test -o ./test/output -annot yes -g ./test/ref.gff -np 1
+
+cons=$(find ./test/output/ -name "samples*masked*fa" | sort | head -1)
+
+if [[ -f $cons ]]; then
+	echo "#######################"
+	echo "Consensus genome found"
+	echo -e "#######################\n"
+else
+	echo "#######################"
+	echo "Consensus genome was not found. Please check the dependencies of QVG."
+	echo -e "#######################\n"
+	exit 1
+fi	
+
+if [[ -f ./test/output/non_haploid_sites.txt ]]; then
+	echo "#######################"
+	echo "Within host variable sites found"
+	echo -e "#######################\n"
+else
+	echo "#######################"
+	echo "Within host variable sites were not found. Please check the dependencies of QVG."
+	echo -e "#######################\n"
+	exit 1
+fi
+
+if [[ -f ./test/output/S1/S1.gff ]]; then
+	echo "#######################"
+	echo "Transferred annotations found"
+	echo -e "#######################\n"
+else
+	echo "#######################"
+	echo "Transferred annotations were not found. Please check the dependencies of QVG."
+	echo -e "#######################\n"
+	exit 1
+fi
+
+echo "TEST RUN IS SUCCESSFUL"
